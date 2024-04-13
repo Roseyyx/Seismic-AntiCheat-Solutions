a76e334681082fd8979af2105e1676ff0b12e84a
Rose <arty@duck.com>
2024-04-13 14:46:45 +0200

    Logging additives

--- Seismic AntiCheat Solutions/Utils/Logger/Logging.h
+++ Seismic AntiCheat Solutions/Utils/Logger/Logging.h
@@ -2,6 +2,7 @@
 
 #include <mutex>
 #include <stdio.h>
+#include <ctime>
 
 enum LogPriority
 {
@@ -18,6 +19,8 @@ class Logger
 private:
 	static LogPriority Priority_;
 	static std::mutex LogMutex_;
+	static const char* FilePath_;
+	static FILE* File_;
 
 public:
 	static void SetPriority(LogPriority NewPriority)
@@ -25,77 +28,107 @@ public:
 		Priority_ = NewPriority;
 	}
 
+	static void EnableFileOutput()
+	{
+		FilePath_ = "log.txt";
+		SetFileOutput();
+	}
+
+	static void EnableFileOutput(const char* NewFilePath)
+	{
+		FilePath_ = NewFilePath;
+		SetFileOutput();
+	}
+
+	static void CloseFileOutput()
+	{
+		FreeFile();
+	}
+
 	template<typename... Args>
 	static void Trace(const char* Message, Args... Arguments)
 	{
-		if (Priority_ <= TracePriority)
-		{
-			std::scoped_lock Lock(LogMutex_);
-			printf("[Trace]\t");
-			printf(Message, Arguments...);
-			printf("\n");
-		}
+		Log("[Trace]\t", TracePriority, Message, Arguments...);
 	}
 
 	template<typename... Args>
 	static void Debug(const char* Message, Args... Arguments)
 	{
-		if (Priority_ <= DebugPriority)
-		{
-			std::scoped_lock Lock(LogMutex_);
-			printf("[Debug]\t");
-			printf(Message, Arguments...);
-			printf("\n");
-		}
+		Log("[Debug]\t", DebugPriority, Message, Arguments...);
 	}
 
 	template<typename... Args>
 	static void Info(const char* Message, Args... Arguments)
 	{
-		if (Priority_ <= InfoPriority)
-		{
-			std::scoped_lock Lock(LogMutex_);
-			printf("[Info]\t");
-			printf(Message, Arguments...);
-			printf("\n");
-		}
+		Log("[Info]\t", InfoPriority, Message, Arguments...);
 	}
 
 	template<typename... Args>
 	static void Warn(const char* Message, Args... Arguments)
 	{
-		if (Priority_ <= WarnPriority)
-		{
-			std::scoped_lock Lock(LogMutex_);
-			printf("[Warn]\t");
-			printf(Message, Arguments...);
-			printf("\n");
-		}
+		Log("[Warn]\t", WarnPriority, Message, Arguments...);
 	}
 
 	template<typename... Args>
 	static void Error(const char* Message, Args... Arguments)
 	{
-		if (Priority_ <= ErrorPriority)
-		{
-			std::scoped_lock Lock(LogMutex_);
-			printf("[Error]\t");
-			printf(Message, Arguments...);
-			printf("\n");
-		}
+		Log("[Error]\t", ErrorPriority, Message, Arguments...);
 	}
 
 	template<typename... Args>
 	static void Critical(const char* Message, Args... Arguments)
 	{
-		if (Priority_ <= CriticalPriority)
+		Log("[Critical]\t", CriticalPriority, Message, Arguments...);
+	}
+private:
+	template<typename... Args>
+	static void Log(const char* MessagePriorityString, LogPriority MessagePriority, const char* Message, Args... Arguments)
+	{
+		if (Priority_ <= MessagePriority)
 		{
+			std::time_t CurrentTime = std::time(0);
+			std::tm* TimeStamp = std::localtime(&CurrentTime);
+			char Buffer[80];
+			strftime(Buffer, 80, "%c", TimeStamp);
+
 			std::scoped_lock Lock(LogMutex_);
-			printf("[Critical]\t");
+
+			printf("%s\t", Buffer);
+			printf(MessagePriorityString);
 			printf(Message, Arguments...);
 			printf("\n");
+
+			if (File_)
+			{
+				fprintf(File_, "%s\t", Buffer);
+				fprintf(File_, MessagePriorityString);
+				fprintf(File_, Message, Arguments...);
+				fprintf(File_, "\n");
+			}
 		}
+	}
+
+	static void SetFileOutput()
+	{
+		if (File_ != 0)
+		{
+			fclose(File_);
+		}
+
+		File_ = fopen(FilePath_, "a");
+
+		if (File_ == 0)
+			printf("Failed to open file at %s", FilePath_);
+	}
+
+	static void FreeFile()
+	{
+		fclose(File_);
+		File_ = 0;
 	}
 };
 
 LogPriority Logger::Priority_ = InfoPriority;
+std::mutex Logger::LogMutex_;
+const char* Logger::FilePath_ = 0;
+FILE* Logger::File_ = 0;
--- Seismic AntiCheat Solutions/main.cpp
+++ Seismic AntiCheat Solutions/main.cpp
@@ -1,12 +1,21 @@
-#include <windows.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "Utils/Logger/Logging.h"
+
+void LogTest(int n)
+{
+	Logger::Info("%d", n);
+}
 
 int main() {
+	Logger::EnableFileOutput();
 
-	while (!GetAsyncKeyState(VK_END))
-	{
+	std::thread Threads[10];
 
-	}
+	for (int i = 0; i < 10; i++) Threads[i] = std::thread(LogTest, i);
+	for (int i = 0; i < 10; i++) Threads[i].join();
 
+	Logger::CloseFileOutput();
 
 	return 0;
 }
