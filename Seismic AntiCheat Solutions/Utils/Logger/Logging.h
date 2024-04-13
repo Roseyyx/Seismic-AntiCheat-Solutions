246f77908e0230e4dee730f78896e4da25fb13ad
Rose <arty@duck.com>
2024-04-13 13:55:48 +0200

    Thread Safe Logging

--- Seismic AntiCheat Solutions/Utils/Logger/Logging.h
+++ Seismic AntiCheat Solutions/Utils/Logger/Logging.h
@@ -1,2 +1,101 @@
 #pragma once
 
+#include <mutex>
+#include <stdio.h>
+
+enum LogPriority
+{
+	TracePriority,
+	DebugPriority,
+	InfoPriority,
+	WarnPriority,
+	ErrorPriority,
+	CriticalPriority,
+};
+
+class Logger
+{
+private:
+	static LogPriority Priority_;
+	static std::mutex LogMutex_;
+
+public:
+	static void SetPriority(LogPriority NewPriority)
+	{
+		Priority_ = NewPriority;
+	}
+
+	template<typename... Args>
+	static void Trace(const char* Message, Args... Arguments)
+	{
+		if (Priority_ <= TracePriority)
+		{
+			std::scoped_lock Lock(LogMutex_);
+			printf("[Trace]\t");
+			printf(Message, Arguments...);
+			printf("\n");
+		}
+	}
+
+	template<typename... Args>
+	static void Debug(const char* Message, Args... Arguments)
+	{
+		if (Priority_ <= DebugPriority)
+		{
+			std::scoped_lock Lock(LogMutex_);
+			printf("[Debug]\t");
+			printf(Message, Arguments...);
+			printf("\n");
+		}
+	}
+
+	template<typename... Args>
+	static void Info(const char* Message, Args... Arguments)
+	{
+		if (Priority_ <= InfoPriority)
+		{
+			std::scoped_lock Lock(LogMutex_);
+			printf("[Info]\t");
+			printf(Message, Arguments...);
+			printf("\n");
+		}
+	}
+
+	template<typename... Args>
+	static void Warn(const char* Message, Args... Arguments)
+	{
+		if (Priority_ <= WarnPriority)
+		{
+			std::scoped_lock Lock(LogMutex_);
+			printf("[Warn]\t");
+			printf(Message, Arguments...);
+			printf("\n");
+		}
+	}
+
+	template<typename... Args>
+	static void Error(const char* Message, Args... Arguments)
+	{
+		if (Priority_ <= ErrorPriority)
+		{
+			std::scoped_lock Lock(LogMutex_);
+			printf("[Error]\t");
+			printf(Message, Arguments...);
+			printf("\n");
+		}
+	}
+
+	template<typename... Args>
+	static void Critical(const char* Message, Args... Arguments)
+	{
+		if (Priority_ <= CriticalPriority)
+		{
+			std::scoped_lock Lock(LogMutex_);
+			printf("[Critical]\t");
+			printf(Message, Arguments...);
+			printf("\n");
+		}
+	}
+};
+
+LogPriority Logger::Priority_ = InfoPriority;
--- Seismic AntiCheat Solutions/main.cpp
+++ Seismic AntiCheat Solutions/main.cpp
@@ -1,13 +1,9 @@
-#include <stdexcept>
 #include <windows.h>
-#include "Detections/Detections.h"
 
 int main() {
 
 	while (!GetAsyncKeyState(VK_END))
 	{
-		if (!Detections::Setup())
-			throw std::runtime_error("Failed to setup detections");
 
 	}
 
